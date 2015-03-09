49eebadb6d3cbe8e1027a40c9e6607d561e54109
rmhorton <rmhorton@attotron.com>
2015-03-08 23:37:06 -0700

    tweaked build_quizdb.R

--- week_06_SQL_from_R/build_quizdb.R
+++ week_06_SQL_from_R/build_quizdb.R
@@ -2,10 +2,13 @@ DATA_DIR <- "/Volumes/data/Courses/USF_stats_programming/"
 library("RCurl")
 library("magrittr")
 
-class_data <- read.delim(file.path(DATA_DIR, "class_data.txt"), sep=";", stringsAsFactors=F)
-source("/Volumes/data/Courses/USF_stats_programming/statprog/week_05_loading_data/quiz_parser.R")
+quiz_parser_url <- "https://raw.githubusercontent.com/rmhorton/statprog/master/week_05_loading_data/quiz_parser.R"
+source(textConnection(getURL(quiz_parser_url)))
 
-raw_url <- github_raw_url(class_data$github_questions[1])
+# get class_data.txt from Canvas
+class_data <- read.delim(file.path(DATA_DIR, "class_data.txt"), sep=";", stringsAsFactors=F)
+
+raw_url <- github_raw_url(class_data$github_questions[1])  # 1 is Bob; try your own row number
 parse_quiz_text(getURL(raw_url))
 
 class_data$num_questions <- 
