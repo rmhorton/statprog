9a4145b5a8ea2ebabe2f5d82863f2ca5a3d9c637
rmhorton <rmhorton@attotron.com>
2015-03-21 00:28:58 -0700

    study guide version 3

--- week_05_loading_data/quiz_parser.R
+++ week_05_loading_data/quiz_parser.R
@@ -1,7 +1,7 @@
 #' Parse markdown-formatted quiz question files into in-memory data structure.
 #' @param quiz_text a block of text to be parsed (not a connection or file, but it can be the output of getUrl).
 #' @NL (default "\n") character or string marking the end of each line of input text.
-#' @example # Read from raw github content
+#' @example Read from raw github content
 #' 	library(RCurl)
 #' 	quizUrl <- "https://raw.githubusercontent.com/rmhorton/hs616/master/questions.md"
 #' 	parse_quiz_text(getURL(quizUrl))
@@ -14,18 +14,26 @@
 #' 				readLines() %>% 
 #' 				paste(collapse="\n") %>% 
 #' 				parse_quiz_text()
-parse_quiz_text <- function(quiz_text, NL="\n"){
+parse_quiz_text <- function(quiz_text){
 	library(magrittr)
 	
-	quiz_text %>%
+	quiz_text %>% fix_miscellaneous_formatting_errors %>%
 	strsplit("## *") %>% .[[1]] %>% .[-1] %>% 
-	strsplit(paste0(NL,NL)) %>%
+	strsplit("\\n[\\n\\s]*\\n") %>%	# split on one or more blank lines
 	lapply(function(q){
-		lecture <- sub("^Lecture *(\\d[ab]).*", "\\1", q[1], ignore.case=T, perl=T)
+		lecture <- sub("^Lecture *(\\d+[ab]).*", "\\1", q[1], ignore.case=T, perl=T)
 		question <- q[2]
-		answers <- strsplit(q[3],paste0(NL,'[*]'))[[1]] %>% sub("^[*] *", "", .)
+		answers <- strsplit(q[3],paste0("\n",'[*]'))[[1]] %>% sub("^[*] *", "", .)
 		list(lecture=lecture, question=question, answers=answers)
 	})
+}
+
+#' Handle certain formatting issues that became apparent after the midterm questions were submitted.
+#' @param quiz_text a block of text to be prepared for parsing
+fix_miscellaneous_formatting_errors <- function(quiz_text){
+	quiz_text %>% gsub("\\r\\n", "\n", .) %>%	# handle Windows LF/CR??
+		gsub("[”]", '\"', .) %>%
+		gsub("Lecture *(\\d+[ab])\\n", "Lecture \\1\n\n", .) # [^\\n]*
 }
 
 # transform the input URL into a raw file URL
