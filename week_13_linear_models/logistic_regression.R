332df1526d0899a5c399eafafaea4355d8731af6
Robert M. Horton, PhD <bhorton@Setups-MacBook-Pro.local>
2015-04-15 00:33:41 -0700

    Logistic regression simulation and slides

--- week_13_linear_models/logistic_regression.R
+++
@@ -1,2 +0,0 @@
-x <- seq(-10, 10, length=100)
-plot(x, 1-1/(1 + exp(x)))
---
+++ week_13_linear_models/logistic_simulation.R
@@ -0,0 +1,31 @@
+logistic <- function(t) 1 / (1 + exp(-t))
+
+N <- 1e6
+df <- data.frame(
+  carcinogens = runif(N, min=0, max=100),
+  age = sample(10:99, N, replace=T)
+)
+
+library(manipulate)
+manipulate({
+  df <- transform(df, score = 10^a * (carcinogens - mean(carcinogens)) + 10^b * (age - mean(age)))
+  df$prob <- logistic(df$score)
+  hist(df$prob, breaks=50)
+}, a=slider(-9, 9, step=0.1, initial = 0), b=slider(-9, 9, step=0.1, initial = 0) )
+
+
+df <- transform(df, score = 10^(-1.1) * (carcinogens - mean(carcinogens)) + 10^-3 * (age - mean(age)))
+df$prob <- logistic(df$score)
+hist(df$prob, breaks=50)
+
+df$cancer <- factor(ifelse(df$prob > runif(N), "Yes", "No"))
+
+fit <- glm(cancer ~ I(carcinogens - mean(carcinogens)) + I(age - mean(age)) - 1, data=df, family="binomial")
+
+plot(df$cancer, predict(fit, type="response"), notch=T)
+
+
+
+# df <- transform(df, score = -0.8 + 0.002 * carcinogens^2 + 0.05 * age )
+# fit <- glm(cancer ~ I(carcinogens^2) + age, data=df, family="binomial")
+
