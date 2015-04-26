fb5ec761019439c551bba3c923d42c37eba2b990
Robert M. Horton, PhD <bhorton@Setups-MacBook-Pro.local>
2015-04-26 01:06:41 -0700

    Now includes intercept
    Score includes non-zero intercept term; outcomes of cancer and no
    cancer are no longer equally likely.

--- week_13_linear_models/logistic_simulation.R
+++ week_13_linear_models/logistic_simulation.R
@@ -8,23 +8,28 @@ df <- data.frame(
 
 library(manipulate)
 manipulate({
-  df <- transform(df, score = 10^a * (carcinogens - mean(carcinogens)) + 10^b * (age - mean(age)))
+  df <- transform(df, 
+  	score = 10^a * (carcinogens - mean(carcinogens)) + 
+  			10^b * (age - mean(age))) - 2
   df$prob <- logistic(df$score)
   hist(df$prob, breaks=50)
 }, a=slider(-9, 9, step=0.1, initial = 0), b=slider(-9, 9, step=0.1, initial = 0) )
+# try a <- -1.4; b <- -1.6
 
-
-df <- transform(df, score = 10^(-1.1) * (carcinogens - mean(carcinogens)) + 10^-3 * (age - mean(age)))
+df <- transform(df, 
+	score = 10^(-1) * (carcinogens - mean(carcinogens)) + 
+			10^(-2) * (age - mean(age))) - 2
 df$prob <- logistic(df$score)
 hist(df$prob, breaks=50)
 
 df$cancer <- factor(ifelse(df$prob > runif(N), "Yes", "No"))
 
-fit <- glm(cancer ~ I(carcinogens - mean(carcinogens)) + I(age - mean(age)) - 1, data=df, family="binomial")
+fit <- glm(cancer ~ I(carcinogens - mean(carcinogens)) + I(age - mean(age)) + 1, 
+		data=df, family="binomial")
 
 plot(df$cancer, predict(fit, type="response"), notch=T)
 
-
+coef(fit)
 
 # df <- transform(df, score = -0.8 + 0.002 * carcinogens^2 + 0.05 * age )
 # fit <- glm(cancer ~ I(carcinogens^2) + age, data=df, family="binomial")
